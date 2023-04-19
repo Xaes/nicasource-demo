9cd4cccbb60e38d8e0a259b6aef3304f99c25ca5
Diego Balmaceda <diego@xaes.dev>
2023-04-19 01:23:48 -0600

    Fixed PM2 script.

--- packages/backend/bootstrap/migrations/20230416221507-initial.cjs
+++ packages/backend/bootstrap/migrations/20230416221507-initial.cjs
@@ -3,8 +3,6 @@ const { DataTypes } = require("sequelize");
 module.exports = {
     up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
     async (transaction) => {
-        // Creating DB.
-        await queryInterface.createDatabase("nicasource");
         // Creating Tables.
         await queryInterface.createTable("creator", {
             id: {
