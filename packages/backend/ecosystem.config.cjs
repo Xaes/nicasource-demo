cff30e38854c2773b8933e3de244ade46e358e12
Diego Balmaceda <diego@xaes.dev>
2023-04-19 00:59:02 -0600

    Fixed PM2 script.

--- packages/backend/ecosystem.config.cjs
+++ packages/backend/ecosystem.config.cjs
@@ -34,7 +34,7 @@ module.exports = {
             ref: "origin/main",
             repo: "https://github.com/Xaes/nicasource-demo.git",
             path: "/home/ubuntu/nicasource-demo",
-            "post-deploy" : "rm -rf node_modules && yarn install && yarn migrate-prod:up && pm2 reload ecosystem.config.cjs --env production",
+            "post-deploy" : "rm -rf node_modules && cd packages/backend/ && yarn install && yarn migrate-prod:up && pm2 reload ecosystem.config.cjs --env production",
             "pre-setup": ""
         }
     }
